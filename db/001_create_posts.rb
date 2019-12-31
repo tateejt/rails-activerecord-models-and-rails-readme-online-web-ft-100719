0ca0d081d4e9106e785dba4578c3f375fdf62a9d
Emery Tate <tateejt@gmail.com>
2019-12-31 01:38:27 +0000

    Automatically backed up by Learn

--- db/001_create_posts.rb
+++ db/001_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :title 
       t.text :description
       
-      t.timestamps null: flase
+      t.timestamps null: false
     end
   end
 end
